a249f18aca09b043d22bf46a4b73e95d7fb90417
camutze <carlosmutzenberg@ufpr.br>
2023-11-09 18:43:54 -0300

    fizpeloalias

--- fibi.asm
+++ fibi.asm
@@ -1,8 +1,50 @@
+#Código da oper	Registrador a 	Registrador b 	Registrador c 	Constante
+
 #assembly code Fibonacci
+# N = r1
+# first = r2
+# next = r3
+# second = r4
+# i = r5
+
+# r0 = 0000
+# r1 = 0001
+# r2 = 0010
+# r3 = 0011
+# r4 = 0100
+# r5 = 0101
+# r6 = 0110
+# r7 = 0111
+# r8 = 1000
+# r9 = 1001
+
+nop 
+addi r0 r0 r1 10 #Defina N como o número desejado de termos da sequência Fibonacci
+
+addi r0 r0 r2 0 #Inicializa first
+
+addi r0 r0 r3 0 #Inicializa next
+
+addi r0 r0 r4 1 #Inicializa second
+
+add  r1 r0 r5  #Inicializa i= N 
 
 
-addi n 
-addi first
-addi second
+#para i = N ate i==0
+
+show r2
+add r2 r4 r3
+add r0 r4 r2
+add r0 r3 r4
+subi r0 r0 r5 -1
+branch 
+
+
+
+
+
+
+
+
 
 show first
--- fibi.c
+++ fibi.c
@@ -2,16 +2,20 @@
 
 int main() {
     int N = 10; // Defina N como o número desejado de termos da sequência Fibonacci
-    int first = 0, second = 1;
+
+    int first = 0;
+    int next = 0;
+    int second = 1;
+    int i = N;
 
     // Exibe o valor de N
     printf("N = %d\n", N);
 
     // Calcula e exibe os primeiros N números da sequência de Fibonacci
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i != 0; i--) {
         printf("%d ", first);
 
-        int next = first + second;
+        next = first + second;
         first = second;
         second = next;
     }
