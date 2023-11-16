0a094fc88ed2743c50b5f8a24693146a74a0367f
Carlos <camutze@gamil.com>
2023-11-16 00:06:55 -0300

    falta relatorio

--- fibi.asm
+++ fibi.asm
@@ -25,31 +25,27 @@
 # r4 = second
 # r5 = i
 # r6 = branch
+# r7 = const 1
     
-nop 
 addi r0 r1 10 #Defina N como o número desejado de termos da sequência Fibonacci
-addi r0 r2 0 #Inicializa first
-addi r0 r3 0 #Inicializa next
-addi r0 r4 1 #Inicializa second
-addi r0 r6 0 #inicializa i
-branch r1 r5 #se r1 for igual a r5 pula
-add r2 r4 r3 # next = first + second;
-add r0 r4 r2 # first = second;
-add r0 r3 r4 #second = next;
-addi r5 r5 1 #
-jump r0 r0 r #linhas
+addi r0 r2 0  #Inicializa first
+addi r0 r3 0  #Inicializa next
+addi r0 r4 1  #Inicializa second
+addi r0 r6 0  #inicializa i
+addi r0 r7 1  #inicializa i_aux
+addi r0 r8 1  #inicializa const 1
+
+loop:
+branch r1 r5  #se r1 for igual a r5 pula
+add r2 r4 r3  # next = first + second;
+add r0 r4 r2  # first = second;
+add r0 r3 r4  #second = next;
+
+addi r5 r7 1  #i = i + 1;
+add r0 r7 r5 #i_aux = i + 1;
+jump r0 r0 r  #linhas
 
 
+aux = i+1
+i = aux;
 
-
-
-
-
-
-
-
-
-
-
-
-show first
--- fibi.c
+++ fibi.c
@@ -2,26 +2,22 @@
 
 int main()
 {
-    int N = 10; // Defina N como o número desejado de termos da sequência Fibonacci
-
+    int N = 10; 
     int first = 0;
     int next = 0;
     int second = 1;
-    int i;
-
-    // Exibe o valor de N
-    printf("N = %d\n", N);
-
-    // Calcula e exibe os primeiros N números da sequência de Fibonacci
-    for (i = 0 ; i < N; i++)
+    int i = 0;
+    while (i < N)
     {
         printf("%d ", first);
-
         next = first + second;
         first = second;
         second = next;
+        i++;
     }
-
-    printf("\n");
     return 0;
 }
+
+
+
+
